38f7b33d74a0e931b246682c5d0bc36de03e8dc3
hotmiaow <hotmiaow@gmail.com>
2026-08-27 12:21:25 -0400

    chore: clean up project structure, remove obsolete scripts, and update build artifacts for watermark tool

--- tdl_download.ps1
+++
@@ -1,9 +0,0 @@
-﻿$url_download = "https://t.me/bjjinstructionalz/"
-
-#tdl.exe download -u  https://t.me/bjjinstructionalz/362
-
-For ($i=271; $i -le 301; $i++) 
-{
-tdl.exe download -u $url_download$i
-Start-Sleep -Seconds 1
-}
--- test_pac.pac
+++
@@ -1,8 +0,0 @@
-if (
- (shExpMatch(host, "hkf1.aaa.com")) ||
-  (shExpMatch(resolved_ip, "10.1.1.1")) ||
-   (shExpMatch(host, "hkf2.aaa.com")) ||
-    (shExpMatch(resolved_ip, "10.1.1.2")))
-    {
-    return "DIRECT";
-    }
